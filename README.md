f5affee1c6336867b08a965f784c8ec72693a804
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-10 11:44:08 +1100

    small addition involving some list comprrhension realated code, ie: iterating through a database and reporting back results based on each entry, an interesting topic that i should know more about and will know more about over the coming lessons. i also fully rigged up the mongo stuff to read data properly. funny how easy it is to do now, some langs make it easier than others, i assume this is why python is such a data friendly lang.  also updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terminal_blog-py
+
+* Just went through list comprehension, which is something I've been lacking personal understanding in for a while now so it was good to be able to have an explanation of how to do it. I'm gonna have to give it a crack in some of my selenium test and see if I can get it to read from some text files or something in order to find all instances and do mass tests recursively.
--- app.py
+++ app.py
@@ -3,3 +3,25 @@ __author__ = 'lw'
 import pymongo
 
 uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collection = database['students']
+
+students = [each['mark'] for each in collection.find({})]
+print(students)
+
+# More ways of doing the same thing:
+# ----------------------------------
+
+# students = collection.find({})
+# student_list = []
+# for student in students:
+#     student_list.append(student)
+#
+#             OR
+#
+# students = collection.find({})
+# for each in students:
+#     print(each)
+
+
