864232dbb44938d1a7c69f8973ae0aee1308755c
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-11 20:09:50 +1100

    so ive butchered the application fairly heavily here, probably could have done this commit across numerous but i was wrtiing code pretty deep in thought. so this commit is mainly to abstract some stuff from posts and into blog, the aim here being to inherit some stuff that is needed but i dont necessarily believe ive done all of this right at this stage to be honest. in fact i know i havent. this commit introduces the blog class and file with some terrible inheritance.

--- models/blog.py
+++ models/blog.py
@@ -5,8 +5,30 @@ from database import Database
 
 class Blog(object):
 
-    def __init__(self, title, author, date_created=datetime.datetime.utcnow(), blog_id=None):
+    def __init__(self, blog_title, author, date_created=datetime.datetime.utcnow(), blog_id=None):
         self.blog_id = uuid.uuid3(uuid.NAMESPACE_DNS, author).hex if blog_id is None else blog_id
-        self.title = title
+        self.blog_title = blog_title
         self.author = author
         self.date_created = date_created
+
+    # inserts posts into mongo collection in json format
+    def save_to_mongo(self):
+        Database.insert(collection='posts',
+                        data=self.json())
+
+    # convert to json for mongo db
+    def json(self):
+        return {
+            'blog_id': self.blog_id,
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+            'created': self.date_created,
+            'post_id': self.post_id
+        }
+
+    @staticmethod
+    # go into db with id param, search thru blog_id for param
+    def from_blog(blog_id):
+        return [blog for blog in Database.find(collection='posts', query={'blog_id': blog_id})]
+
